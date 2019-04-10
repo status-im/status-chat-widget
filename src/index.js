69c077dc0327a655950812bbb7a44f56a2b18800
Iuri Matias <iuri.matias@gmail.com>
2019-04-10 15:55:58 -0400

    generate chat widget

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
 import StatusJS from 'status-js-api';
 import Murmur from 'murmur-client';
 
-let server = new Murmur({
-  protocols: ["libp2p"],
-  // signalServer: { host: '104.248.64.24', port: '9090', protocol: 'ws' },
-  signalServers: [
-    //"/dns4/cryptolife.status.im/tcp/443/wss/p2p-webrtc-star"
-    "/dns4/web-bridge.status.im/tcp/443/wss/p2p-webrtc-star"
-  ],
-  bootnodes: []
-});
-server.start();
+window.StatusWidget = function (channelName) {
+  var chatBox = document.createElement('div');
+  chatBox.id = "chat";
 
-const status = new StatusJS();
-status.connectToProvider(server.provider, null);
+  var chatInput = document.createElement('input');
+  chatInput.type = "input";
+  chatInput.id = "post";
 
-console.dir("hello world!")
+  document.querySelectorAll("#status-chat-widget")[0].append(chatBox);
+  document.querySelectorAll("#status-chat-widget")[0].append(chatInput);
+
+  let server = new Murmur({
+    protocols: ["libp2p"],
+    signalServers: [
+      "/dns4/web-bridge.status.im/tcp/443/wss/p2p-webrtc-star"
+    ],
+    bootnodes: []
+  });
+  server.start();
+
+  const status = new StatusJS();
+  status.connectToProvider(server.provider, null);
+
+  status.joinChat(channelName, () => {
+    status.onMessage(channelName, (err, data) => {
+      const msg = JSON.parse(data.payload)[1][0];
+
+      const message = { username: data.username, message: msg, pubkey: data.data.sig, data };
+      let div = document.createElement('div');
+      div.innerHTML = message.username + "> " + message.message;
+      document.querySelectorAll("#chat")[0].append(div)
+    })
+
+    var input = document.getElementById("post")
+
+    input.addEventListener("keyup", function(event) {
+      if (event.keyCode !== 13) {
+        return
+      }
+
+      event.preventDefault();
+
+      var value = document.getElementById("post").value;
+      status.sendMessage(channelName, value);
+      document.getElementById("post").value = "";
+    });
+  })
+}
