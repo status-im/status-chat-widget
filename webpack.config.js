fbab9d84960d0c5c8490ebd3e407230af1c281f2
Iuri Matias <iuri.matias@gmail.com>
2019-04-10 15:59:20 -0400

    rename generated dist file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   target: "web",
   output: {
     path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    filename: "statuswidget.js"
   },
   resolve: {
     extensions: [".js"]
